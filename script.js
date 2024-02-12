ee24a6d44a549615e18300820f11e5c9b4f216dc
SASIKIRANMAI <iravarajusasi123@gmail.com>
2024-02-12 12:14:16 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const navBtn = document.getElementById("nav-bth");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+navBtn.addEventListener("click", () => {navbar.classList.add("showNav")});
+navClose.addEventListener("click", () => {navbar.classList.remove("showNav")})
